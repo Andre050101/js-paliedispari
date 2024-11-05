48e53388fd68deffde5d6306109e5a8fa028fa6f
Andre050101 <agiancristiano90@gmail.com>
2024-11-05 15:17:09 +0100

    word request added to script.js

---
+++ palindroma/script/script.js
@@ -0,0 +1,9 @@
+/*Consegna:
+1- Chiedere all’utente di inserire una parola
+2- Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+/*Richiesta parola a utente: */
+let word = prompt("Inserire la parola per la quale verificare la palindromicità:")
+
+
