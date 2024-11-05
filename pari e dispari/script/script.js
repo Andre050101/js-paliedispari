a13a3e7e468a25086ca30b001acc460d795a34e4
Andre050101 <agiancristiano90@gmail.com>
2024-11-05 16:03:30 +0100

    Function randomN(min, max) and sumEvenOdd(n) done, win or lose control done, exercise completed

--- pari e dispari/script/script.js
+++ pari e dispari/script/script.js
@@ -8,7 +8,45 @@ Dichiariamo chi ha vinto.
 
 */
 
-let pariDispari = prompt("Scegli: pari o dispari:");
-while (pariDispari !=="pari" && pariDispari !=="dispari"){
+let pariDispari = prompt("Scegli: pari o dispari:"); //Richiesta pari o dispari
+pariDispari = pariDispari.toLowerCase(); //Conversione in minuscolo per controllo validazione
+while (pariDispari !=="pari" && pariDispari !=="dispari"){ //Validazione/Controllo inserimento
     pariDispari = prompt("Errore! Scelta invalida! Scegli: pari o dispari:");
 }
+
+let nUser = parseInt(prompt("Scegli un numero da 1 a 5:")); //Richiesta numero da 1 a 5
+while(isNaN(nUser) || nUser < 1 || nUser > 5){ //Validazione/Controlo numero inserito
+    nUser = parseInt(prompt("Numero non valido. Scegli un numero da 1 a 5:"));
+} 
+
+/**
+ * Genera numero random intero in range (min, max)
+ * 
+ * @param {number} min //Numero minimo range
+ * @param {number} max //Numero massimo range
+ * @returns {number} //Numero restituito compreso nel range
+ */
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let nPC = randomNumber(1, 5); //Numero random PC
+let somma = nUser + nPC; //Somma numero utente e numero pc
+
+/**
+ * Controlla se il numero dato come argomento è pari o dispari e ne restituisce la riposta
+ * 
+ * @param {number} number //Numero per la quale verificare se è pari o dispari
+ * @returns {string} //"pari" se number è pari, dispari altrimenti 
+ */
+function sumEvenOdd(number){
+    if(number % 2 === 0)
+        return "pari";
+    else
+        return "dispari";
+}
+
+if(sumEvenOdd(somma)===pariDispari)
+    console.log("Hai vinto!");
+else
+    console.log("Hai perso!");
